89b310d498ca7f74ea1009419963c533fbb4089e
cmb69 <cmbecker69@gmx.de>
2014-10-12 21:23:23 +0000

    added CSS classes for easier styling of the default view (suggested by Der Zwerch and Ralf H.)

--- views/default.php
+++ views/default.php
@@ -1,21 +1,21 @@
 <?php $this->preventAccess()?>
 <!-- Feedview_XH: default feed view -->
 <div class="feedview_header">
-    <h4>
-        <a href="<?php echo $feed->get_permalink();?>" target="_blank">
-            <?php echo $feed->get_title();?>
+    <h4 class="feedview_feed_title">
+        <a href="<?php echo $feed->get_permalink()?>" target="_blank">
+            <?php echo $feed->get_title()?>
         </a>
     </h4>
-    <p><?php echo $feed->get_description();?></p>
+    <div class="feedview_feed_description"><?php echo $feed->get_description()?></div>
 </div>
 <?php foreach ($feed->get_items(0, (int) $pcf['default_items']) as $item):?>
 <div class="feedview_item">
-    <h5>
+    <h5 class="feedview_item_title">
         <a href="<?php echo $item->get_permalink();?>" target="_blank">
-            <?php echo $item->get_title();?>
+            <?php echo $item->get_title()?>
         </a>
     </h5>
-    <p><?php echo $item->get_description();?></p>
-    <p><small><?php echo sprintf($ptx['message_posted'], $item->get_date($ptx['format_date']));?></small></p>
+    <div class="feedview_item_description"><?php echo $item->get_description()?></div>
+    <p class="feedview_item_posted"><?php echo sprintf($ptx['message_posted'], $item->get_date($ptx['format_date']))?></p>
 </div>
-<?php endforeach;?>
+<?php endforeach?>
