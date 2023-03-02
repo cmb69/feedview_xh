0d1d6983b754ef9fb75f7260a4176c65252996d5
Christoph M. Becker <cmbecker69@gmx.de>
2023-03-02 13:00:30 +0100

    Adopt PHPUnit

---
+++ tests/bootstrap.php
@@ -0,0 +1,17 @@
+<?php
+
+spl_autoload_register(function (string $className) {
+    $parts = explode("\\", $className);
+    if ($parts[0] !== "Feedview") {
+        return;
+    }
+    if (count($parts) === 3) {
+        $parts[1] = strtolower($parts[1]);
+    }
+    $filename = implode("/", array_slice($parts, 1));
+    if (is_readable("./classes/$filename.php")) {
+        include_once "./classes/$filename.php";
+    } elseif (is_readable("./tests/$filename.php")) {
+        include_once "./tests/$filename.php";
+    }
+});
