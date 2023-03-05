c3d815785ceea5fcd10ce1228552712df00f3d70
Christoph M. Becker <cmbecker69@gmx.de>
2023-03-05 23:32:55 +0100

    Back to dev

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,7 +2,7 @@
 
 const CMSIMPLE_XH_VERSION = "CMSimple_XH 1.7.5";
 const CMSIMPLE_URL = "http://example.com/";
-const FEEDVIEW_VERSION = "1.1";
+const FEEDVIEW_VERSION = "1.2-dev";
 
 require_once "../../cmsimple/functions.php";
 
